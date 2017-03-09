40d2f27a7ecd07bc0eeb4caf83911d5db42658a0
Philippe Pinceloup <ph.pinceloup@gmail.com>
2017-03-09 21:03:59 +0100

    FR, degrés et icone

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 var callApi = function() {
     town = document.getElementById('ville').value;
-    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + town + "&APIKEY=ca9770e3fc2bf89e08cc301b819fb83a&lang=fr";
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + town + "&APIKEY=ca9770e3fc2bf89e08cc301b819fb83a&lang=fr&units=metric";
     req.open('GET', url, false);
     req.send(null);
 };
@@ -12,8 +12,12 @@ req.onreadystatechange = function() { // cette fonction s'exécute à chaque foi
         if (req.status == 200) {
             console.log(req.responseText);
             var response = JSON.parse(req.responseText); // converti JSON en objet JS
+            let iconCode = response.weather[0].icon;
+            let weatherIcon = 'http://openweathermap.org/img/w/' + iconCode + '.png';
             var description = response.weather[0].description; // accède à l'objet
-            document.getElementById("weather").innerHTML = description;
+            document.getElementById("weather").innerHTML = "Le temps à " + town + " est actuellement " + description + "<img src=" + weatherIcon + ">";
+            var temp = response.main.temp; // accède à l'objet
+            document.getElementById("temp").innerHTML = " et la température est de " + Math.round(temp) + "°C.";
         }
     }
 };
